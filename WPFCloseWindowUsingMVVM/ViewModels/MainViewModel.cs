791db45a64593a1eaee364d862a3632b80143529
GregoryE <GregoryE@GGSP006470>
2023-09-22 21:01:41 +0800

    Add project files.

---
+++ WPFCloseWindowUsingMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WPFCloseWindowUsingMVVM.ViewModels
+{
+    public partial class MainWindowViewModel : clsViewModelBase
+    {
+        private ICommand pClosingWindowCommand;
+        public ICommand ClosingWindowCommand
+        {
+            get
+            {
+                return pClosingWindowCommand ?? (pClosingWindowCommand = new DelegateCommand(fnCloseWindow));
+            }
+        }
+
+        public void fnCloseWindow(object obj)
+        {
+            Window window = (Window)obj;
+            window.Close();
+        }
+    }
+}
